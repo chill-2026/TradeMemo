6b02578d487be7ff51970ad385636326ed4bd335
chill-2026 <h76650383@gmail.com>
2026-06-08 18:07:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,28 @@
 const CACHE_NAME = 'trade-memo-v1';
 const ASSETS = [
-  './index.html'
+  './index.html',
+  './tailwindcss.js',
+  './lucide.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
